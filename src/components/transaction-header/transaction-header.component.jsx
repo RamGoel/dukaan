4d4829c159bd9a80d879c6878abbc8187c195f62
Ram Goel <rgoel766@gmail.com>
2024-01-08 20:59:00 +0530

    style: modified dropdown

--- src/components/transaction-header/transaction-header.component.jsx
+++ src/components/transaction-header/transaction-header.component.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
+import SelectArrowIcon from '../../media/icons/SelectArrowIcon'
 
-const options = [
-  "Last Month",
-  // "Last 3 Months",
-  // "Last 6 Months",
-  // "Last Year"
-]
 const TransactionHeader = () => {
   return (
     <div className=' p-3 w-full flex my-3 items-center justify-between'>
       <h3 className='text-lg font-semibold'>Overview</h3>
-      <select className='p-[5px] text-sm text-gray-500 rounded-md focus-visible:outline-none bg-transparent border-[1.5px]'>
-        {
-          options.map(item => <option className='text-sm p-[5px]'>{item}</option>)
-        }
-      </select>
+      <div className='h-[32px] opacity-80 flex items-center justify-between border-[1.5px] rounded-md px-2 py-1 '>
+        <p className='text-xs mr-2'>Last Month</p>
+        <SelectArrowIcon size={10} />
+      </div>
     </div>
   )
 }
