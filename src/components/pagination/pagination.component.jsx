ae5c44cb68bb33a49f6176872e71d4b0c8bd5ea4
Ram Goel <rgoel766@gmail.com>
2024-01-08 22:13:59 +0530

    chore: map in pagination

--- src/components/pagination/pagination.component.jsx
+++ src/components/pagination/pagination.component.jsx
@@ -1,41 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'tailwindcss/tailwind.css';
-import DownArrowIcon from '../../media/icons/DownArrowIcon';
 import SelectArrowIcon from '../../media/icons/SelectArrowIcon';
 
 const Pagination = () => {
+    const [page, setPage] = useState(11)
+    const values = [1, '...', 11, 12, 13, 14, 15, 16, 17]
     return (
         <ul className="flex items-center w-fit my-6 mx-auto">
             <li>
                 <button className=" text-black border-[1.5px] hover:bg-gray-100 flex items-center justify-center rounded px-3 text-xs py-2"><SelectArrowIcon customClass='rotate-90 mr-2' size={10} /> Previous</button>
             </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">1</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">...</button>
-            </li>
-            <li>
-                <button className=" text-white rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs bg-blue-900">11</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">12</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">13</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">14</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">15</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">16</button>
-            </li>
-            <li>
-                <button className=" text-black rounded px-2 py-2 mx-1 hover:bg-gray-100 text-xs">17</button>
-            </li>
+            {
+                values.map(item => {
+                    return <li onClick={()=>setPage(item)}>
+                        <button className={`rounded w-[30px] h-[30px] mx-1  text-xs ${page === item ? ' bg-blue-900 text-white' : 'hover:bg-gray-100 text-black'}`}>{item}</button>
+                    </li>
+                })
+            }
             <li>
                 <button className=" text-black border-[1.5px] hover:bg-gray-100 flex items-center justify-center rounded px-3 text-xs py-2">Next <SelectArrowIcon customClass='-rotate-90 ml-2' size={10} /></button>
             </li>
