1dccad150953644a1a7f820798ece23730bee42e
Ram Goel <rgoel766@gmail.com>
2024-01-08 17:28:49 +0530

    feat: added tailwind

---
+++ src/components/sidebar/sidebar.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='h-screen w-1/5 bg-blue-950'>  
+      
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/layout/dashboard/dashboard.layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/sidebar.component'
+
+const DashboardLayout = () => {
+    return (
+        <div>
+            <div>
+                <Sidebar />
+            </div>
+        </div>
+    )
+}
+
+export default DashboardLayout
